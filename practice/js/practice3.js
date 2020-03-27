94daad32a53bdec0efbcb58a1bc54cc03d9c24d8
Akshit Sethi <akshitsethi@gmail.com>
2020-03-27 21:24:46 +0530

    Add files for the game. Do everything on my own to code the game

---
+++ practice/js/practice3.js
@@ -0,0 +1,29 @@
+/**
+ * GAME RULES
+ * 
+ * The game has 2 players, playing in rounds
+ *
+ * In each turn, a player rolls a dice as many times as he wishes. Each result get added
+ * to this ROUND score
+ *
+ * BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next
+ * player's turn
+ *
+ * The player can choose to 'Hold', which means that his ROUND score gets added to his
+ * GLOBAL score. After that, it's the next player's turn
+ *
+ * The first player to reach 100 points on GLOBAL score wins the game
+ * 
+ * CODING Challenges
+ * 
+ * #1 A player looses his ENTIRE score whe he rolls two 6 in a row. After that, it's the 
+ * next player's turn.
+ * 
+ * #2 Add an input field to the HTML where players can set the winning score, so that they
+ * can change the predefined score of 100.
+ * 
+ * #3 Add another dice to the game, so that there are two dices now. The player looses his
+ * current score if one of them is a 1.
+ */
+
+var scores, roundScore, activePlayer, gamePlaying;
