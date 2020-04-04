11afffb486487d895a0ad8a38345244e655c349c
Akshit Sethi <akshitsethi@gmail.com>
2020-04-04 20:01:17 +0530

    Learned first class functions

--- practice/js/practice4.js
+++ practice/js/practice4.js
@@ -62,3 +62,105 @@ var sun = Object.create(objProto, {
 console.log(sun.name);
 
 // Moving ahead to primitives va Objects
+// For primitives (string, number, boolean, undefined, null)
+// a new copy is created in the memory while for objects only the reference in the 
+// memory is passed
+var a = 30;
+var b = a;
+
+a = 50;
+
+// Will output 30 because a new copy has been created
+console.log(b);
+
+// For objects
+var obj1 = {
+  name: 'John',
+  age: 32
+};
+
+var obj2 = obj1;
+
+obj1.name = 'Mark';
+
+// Will output "Mark" because for objects, only reference to the object in the memory is passed
+console.log(obj2.name);
+
+// Same logic applies to functions for both primitives and objects
+function change(a, b) {
+  age = a;
+  obj1.name = b;
+}
+
+change(100, 'Steve');
+console.log(b);
+console.log(obj2.name);
+
+// First class functions
+// Functions within functions
+var arr1 = ['designer', 'teacher', 'programmer', 'designer', 'entrepreneur'];
+var arr2 = [1980, 1987, 1943, 1969, 2000];
+
+// Here, first param passes is the array
+// and the second one is the function
+var example1 = function(arr, fn) {
+  var newArr = [];
+
+  if (arr.length > 0) {
+    arr.forEach(function(element) {
+      newArr.push(fn(element));
+    });
+  }
+
+  console.log(newArr);
+}
+
+var func1 = function(el) {
+  switch (el) {
+    case 'designer':
+      return 'What is the new design you are working on?';
+    case 'teacher':
+      return 'Which topic are you going to teach today?';
+    case 'programmer':
+      return 'How many bugs have you fixed today?';
+    default:
+      return 'What the fuck are you working on today';
+  }
+}
+
+var func2 = function(el) {
+  return 2020 - el;
+}
+
+example1(arr1, func1);
+example1(arr2, func2);
+
+// Return function with function
+var example2 = function(el) {
+  switch (el) {
+    case 'designer':
+      return function(name) {
+        console.log(name + ', what is the new design you are working on?');
+      }
+    case 'teacher':
+      return function(name) {
+        console.log(name + ', which topic are you going to teach today?');
+      }
+    case 'programmer':
+      return function(name) {
+        console.log(name + ', how many bugs have you fixed today?');
+      }
+    default:
+      return function(name) {
+        console.log(name + ', what the fuck are you working on today?');
+      }
+  }
+}
+
+// One of calling it is
+var call1 = example2('programmer');
+call1('Akshit');
+
+// Another way of calling the function
+example2('designer')('Akshit');
+example2('entrepreneur')('Akshit');
